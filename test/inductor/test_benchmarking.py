d1193c388e31cc75e68578b59d73b10f0305ad9b
Nicolas Macchioni <nmacchioni@fb.com>
2024-08-21 15:28:30 -0700

    basic InductorBenchmarker
    ghstack-source-id: dcea48b3a84a7186c1dc3d9007783186d2ac4ed1
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/133058

--- test/inductor/test_benchmarking.py
+++ test/inductor/test_benchmarking.py
@@ -4,7 +4,13 @@ import unittest
 
 import torch
 from torch._dynamo.utils import counters
-from torch._inductor.runtime.benchmarking import Benchmarker, TritonBenchmarker
+from torch._inductor import config
+from torch._inductor.runtime.benchmarking import (
+    is_feature_enabled,
+    Benchmarker,
+    InductorBenchmarker,
+    TritonBenchmarker,
+)
 from torch._inductor.test_case import run_tests, TestCase
 from torch.testing._internal.common_utils import (
     decorateIf,
@@ -17,6 +23,7 @@ from torch.testing._internal.inductor_utils import GPU_TYPE, HAS_CPU, HAS_GPU
 ALL_BENCHMARKER_CLASSES = (
     Benchmarker,
     TritonBenchmarker,
+    InductorBenchmarker,
 )
 
 
@@ -101,12 +108,64 @@ class TestBenchmarker(TestCase):
     @parametrize("benchmarker_cls", ALL_BENCHMARKER_CLASSES)
     def test_benchmark_safely_infers_device_many_devices(self, benchmarker_cls):
         benchmarker = benchmarker_cls()
-        (fn, cpu_args, cpu_kwargs), _ = self.make_sum("cpu")
-        (_, gpu_args, gpu_kwargs), _ = self.make_sum(GPU_TYPE)
+        (fn, cpu_args, cpu_kwargs), _ = self.make_params("cpu")
+        (_, gpu_args, gpu_kwargs), _ = self.make_params(GPU_TYPE)
         many_devices_args = cpu_args + gpu_args
         many_devices_kwargs = cpu_kwargs
         many_devices_kwargs.update(gpu_kwargs)
         benchmarker.benchmark(fn, many_devices_args, many_devices_kwargs)
+
+    @unittest.skipIf(config.is_fbcode(), "test does not run in fbcode")
+    @parametrize("feature_name", ("inductor_benchmarker",))
+    @parametrize(
+        "config_name,config_val,expected",
+        [
+            ("env_val", "1", False),
+            ("env_val", "0", True),
+            ("env_val", "", None),
+            ("oss_default", True, False),
+            ("oss_default", False, True),
+        ],
+    )
+    def test_is_feature_enabled(
+        self,
+        feature_name,
+        config_name,
+        config_val,
+        expected,
+    ):
+        @config.patch({f"benchmarking.{feature_name}.{config_name}": config_val})
+        def inner():
+            return is_feature_enabled(feature_name)
+
+        if expected is not None:
+            self.assertEqual(inner(), expected)
+        else:
+            self.assertEqual(
+                inner(),
+                getattr(config.benchmarking, self.feature_name).oss_default,
+            )
+
+    @unittest.skipIf(not HAS_GPU, "requires GPU")
+    @parametrize("benchmarker_cls", (InductorBenchmarker,))
+    @parametrize("should_fallback", (True, False))
+    def test_benchmark_gpu_fallback(
+        self, benchmarker_cls, should_fallback, device=GPU_TYPE
+    ):
+        benchmarker = benchmarker_cls()
+        _, _callable = self.make_params(device)
+        if should_fallback:
+            benchmarker.should_fallback = lambda: True
+            _ = benchmarker.benchmark_gpu(_callable)
+            self.assertEqual(
+                self.counter_value(InductorBenchmarker, "triton_do_bench"), 1
+            )
+            self.assertEqual(
+                self.counter_value(InductorBenchmarker, "benchmark_gpu"), 1
+            )
+        else:
+            benchmarker.should_fallback = lambda: False
+            self.assertEqual(self.counter_value(benchmarker_cls, "benchmark_gpu"), 1)
 
 
 if __name__ == "__main__":
--- torch/_inductor/runtime/benchmarking.py
+++ torch/_inductor/runtime/benchmarking.py
@@ -1,3 +1,4 @@
+import inspect
 import time
 from functools import cached_property, wraps
 from itertools import chain
@@ -7,6 +8,7 @@ from typing_extensions import Concatenate, ParamSpec, Self, TypeVar
 
 import torch
 from torch._dynamo.utils import counters
+from torch._inductor.config import benchmarking as benchmarking_config, is_fbcode
 
 
 logger = torch._logging.getArtifactLogger(__name__, "benchmarking")
@@ -158,6 +160,7 @@ class Benchmarker:
         run_for(warmup)
         return median(run_for(rep))
 
+    @maybe_time
     @count
     def benchmark_gpu(self: Self, *args: Any, **kwargs: Any) -> float:
         raise NotImplementedError
@@ -194,6 +197,16 @@ class TritonBenchmarker(Benchmarker):
         this is the first requested quantile. Else, if `kwargs["return_mode"]` is specified,
         this is the requested return mode. Otherwise, this is the median.
         """
+
+        # this method may be used as a fallback if certain benchmarking features are disabled,
+        # in which case those requests may contain kwargs that are not specific to Triton's
+        # `do_bench_gpu`. as such, we may need to prune out kwargs that do not exists in
+        # `do_bench_gpu`'s signature.
+        do_bench_params = inspect.signature(self.triton_do_bench).parameters
+        for kwarg in list(kwargs.keys()):
+            if kwarg not in do_bench_params:
+                del kwargs[kwarg]
+
         if "quantiles" in kwargs:
             return self.triton_do_bench(_callable, **kwargs)[0]
         elif "return_mode" in kwargs:
@@ -201,4 +214,172 @@ class TritonBenchmarker(Benchmarker):
         return self.triton_do_bench(_callable, **kwargs, return_mode="median")
 
 
-benchmarker = TritonBenchmarker()
+def is_feature_enabled(feature_name: str) -> bool:
+    """Method to decide whether or not a feature is enabled. For more context, see the
+    benchmarking configuration section in `torch._inductor.config.benchmarking`.
+    """
+    feature_config = getattr(benchmarking_config, feature_name)
+    if feature_config.env_val is not None:
+        if feature_config.env_val == "1":
+            return True
+        elif feature_config.env_val == "0":
+            return False
+    if not is_fbcode():
+        return feature_config.oss_default
+    if feature_config.local_version is not None:
+        return (
+            feature_config.local_version
+            >= torch._utils_internal.justknobs_getval_int(
+                f"pytorch/benchmarking:{feature_name.upper()}_VERSION"
+            )
+        )
+    return False
+
+
+def maybe_fallback(
+    fn: Callable[Concatenate[Any, P], T]
+) -> Callable[Concatenate[Any, P], T]:
+    """Wrapper that controls feature fallbacks. It is expected that `fn` is a
+    method of one of `Benchmarker`'s subclasses with a corresponding `feature_name`
+    attribute; typing limitations prevent us from declaring this directly. When
+    `fn` is called, in the form `fn(self, ...)`, we will check that the feature
+    `self.feature_name` is enabled; if the feature `feature_name` is not enabled,
+    we will fallback to the parent class' implementation of `fn`.
+    """
+
+    @wraps(fn)
+    def wrapper(self: Any, *args: P.args, **kwargs: P.kwargs) -> T:
+        if not is_feature_enabled(self.feature_name):
+            fallback_fn = getattr(super(self.__class__, self), fn.__name__)
+            counters["inductor"]["benchmarking." + self.feature_name + ".disabled"] += 1
+            logger.debug(
+                "Feature `%s` is disabled, `benchmarking.%s.%s` will fallback to `benchmarking.%s.%s`.",
+                self.feature_name,
+                self.__class__.__name__,
+                fn.__name__,
+                self.__class__.__base__.__name__,
+                fallback_fn.__name__,
+            )
+            return fallback_fn(*args, **kwargs)
+        return fn(self, *args, **kwargs)
+
+    return wrapper
+
+
+class InductorBenchmarker(TritonBenchmarker):
+    feature_name = "inductor_benchmarker"
+
+    @cached_property
+    def L2_cache_size(self: Self) -> int:
+        """Get the L2 cache size, in bytes, of the current device."""
+        device = torch.cuda.current_device()
+        props = torch.cuda.get_device_properties(device)
+        return props.L2_cache_size
+
+    def get_event_pairs(
+        self: Self, iters: int
+    ) -> List[Tuple[torch.cuda.Event, torch.cuda.Event]]:
+        """Get `iters` pairs of CUDA events."""
+        return [
+            (
+                torch.cuda.Event(enable_timing=True),
+                torch.cuda.Event(enable_timing=True),
+            )
+            for _ in range(iters)
+        ]
+
+    def get_event_pairs_min_timing(
+        self: Self, event_pairs: List[Tuple[torch.cuda.Event, torch.cuda.Event]]
+    ) -> float:
+        """Get the minimum timing, in milliseconds, for a group of CUDA event pairs."""
+        return min(
+            [
+                start_event.elapsed_time(end_event)
+                for start_event, end_event in event_pairs
+            ]
+        )
+
+    @maybe_fallback
+    @maybe_time
+    @count
+    def benchmark_gpu(
+        self: Self,
+        _callable: Callable[[], Any],
+        estimation_iters: int = 5,
+        memory_warmup_iters: int = 100,
+        benchmark_iters: int = 100,
+        max_benchmark_duration: int = 25,
+        **kwargs: Any,
+    ) -> float:
+        """Benchmark a GPU callable using a custom benchmarking implementation.
+
+        Arguments:
+        - _callable: The callable to benchmark.
+
+        Keyword Arguments:
+        - estimation_iters: Optionally, the number of iterations to run `_callable`
+        during runtime estimation.
+        - memory_warmup_iters: Optionally, the number of iterations to flush the L2
+        cache before starting benchmarking.
+        - benchmark_iters: Optionally, the number of iterations to run `_callable`
+        during the benchmarking.
+        - max_benchmark_duration: Optionally, the maximum duration of the benchmarking,
+        in milliseconds. An estimated duration is calculated based on the values
+        of `memory_warmup_iters` and `benchmark_iters`, along with the estimated
+        runtime of `_callable` and various other factors, and we then shrink
+        `benchmark_iters` to fit in the alloted maximum duration.
+        - **kwargs: Additional kwargs that may be passed to the fallback.
+
+        Returns:
+        - The minimum runtime of `_callable`, in milliseconds.
+        """
+        # we don't want any outside errors propagating into benchmarking
+        torch.cuda.synchronize()
+
+        # warmup `_callable` (and catches any failures in the process)
+        _callable()
+        torch.cuda.synchronize()
+
+        # see https://github.com/triton-lang/triton/pull/840 for why `dtype=torch.int`
+        buffer = torch.empty(self.L2_cache_size // 4, dtype=torch.int, device="cuda")
+        buffer.zero_()
+
+        # estimate the runtime of `_callable`
+        event_pairs = self.get_event_pairs(estimation_iters)
+        for start_event, end_event in event_pairs:
+            buffer.zero_()
+            start_event.record()
+            _callable()
+            end_event.record()
+        torch.cuda.synchronize()
+        estimated_timing = self.get_event_pairs_min_timing(event_pairs)
+
+        # adjust `benchmark_iters` to fit in the maximum benchmarking duration
+        benchmark_iters = max(
+            min(benchmark_iters, int(max_benchmark_duration // estimated_timing)), 1
+        )
+
+        # do the memory warmup
+        for _ in range(memory_warmup_iters):
+            buffer.zero_()
+
+        # benchmark `_callable`
+        event_pairs = self.get_event_pairs(benchmark_iters)
+        for start_event, end_event in event_pairs:
+            buffer.zero_()
+            start_event.record()
+            _callable()
+            end_event.record()
+        torch.cuda.synchronize()
+        benchmarked_timing = self.get_event_pairs_min_timing(event_pairs)
+
+        # explicitly delete the buffer, sometimes helps memory
+        # footprint metrics in OSS Inductor performance benchmarks
+        del buffer
+
+        # return the minimum of `estimated_timing` and `benchmarked_timing`,
+        # we just want the minimum timing overall so we might as well check both
+        return min(estimated_timing, benchmarked_timing)
+
+
+benchmarker = InductorBenchmarker()
