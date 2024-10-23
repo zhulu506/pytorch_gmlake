83c0d3c12741434777e90380303ae4934f436ede
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-10-23 16:58:35 +0800

    [BE][Easy] Migrate code formatter for `torch/[e-n]*/` from `black` to `ruff format`
    ghstack-source-id: a77ca8c60b4cbae2bd4f4c1771f39efeeacc2f26
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/138442

--- torch/export/dynamic_shapes.py
+++ torch/export/dynamic_shapes.py
@@ -599,7 +599,7 @@ class ShapesCollection:
     Used to assign dynamic shape specifications to tensors that appear in inputs.
 
     Example::
-        args = ({"x": tensor_x, "others": [tensor_y, tensor_z]})
+        args = {"x": tensor_x, "others": [tensor_y, tensor_z]}
 
         dim = torch.export.Dim(...)
         dynamic_shapes = torch.export.ShapesCollection()
@@ -879,7 +879,8 @@ def _process_dynamic_shapes(
                 i,
                 dim.__name__,
                 StrictMinMaxConstraint(
-                    vr=ValueRanges(lower=dim.value, upper=dim.value), warn_only=False  # type: ignore[attr-defined]
+                    vr=ValueRanges(lower=dim.value, upper=dim.value),  # type: ignore[attr-defined]
+                    warn_only=False,
                 ),
             )
         else:
@@ -889,7 +890,8 @@ def _process_dynamic_shapes(
                 i,
                 dim.__name__,
                 StrictMinMaxConstraint(
-                    vr=ValueRanges(lower=dim.min, upper=dim.max), warn_only=False  # type: ignore[attr-defined]
+                    vr=ValueRanges(lower=dim.min, upper=dim.max),  # type: ignore[attr-defined]
+                    warn_only=False,
                 ),
             )
         return constraint
@@ -969,7 +971,7 @@ def _process_dynamic_shapes(
 
 
 def _get_dim_name_mapping(
-    dynamic_shapes: Union[Dict[str, Any], Tuple[Any], List[Any], None]
+    dynamic_shapes: Union[Dict[str, Any], Tuple[Any], List[Any], None],
 ):
     name_to_dim = {}
     for dim in tree_flatten(
