d7ef6801d3ea7af145df47d171133048a3e7a002
Xuehai Pan <XuehaiPan@pku.edu.cn>
2024-10-29 20:57:10 +0800

    [dynamo] support `functools.cmp_to_key`
    ghstack-source-id: 83cc576000399e0c855d80fceee3b49579a580ce
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/133805

--- torch/_dynamo/polyfills/functools.py
+++ torch/_dynamo/polyfills/functools.py
@@ -2,10 +2,16 @@
 Python polyfills for functools
 """
 
+from __future__ import annotations
+
 import functools
-from typing import Callable, Iterable, TypeVar
+from typing import Callable, Iterable, TYPE_CHECKING, TypeVar
 
 from ..decorators import substitute_in_graph
+
+
+if TYPE_CHECKING:
+    from _typeshed import SupportsAllComparisons
 
 
 __all__ = ["reduce"]
@@ -44,3 +50,34 @@ def reduce(
         value = function(value, element)
 
     return value
+
+
+# Copied from functools.py in the standard library
+@substitute_in_graph(functools.cmp_to_key)
+def cmp_to_key(
+    mycmp: Callable[[_T, _T], int],
+) -> Callable[[_T], SupportsAllComparisons]:
+    class K:
+        __slots__ = ("obj",)
+
+        def __init__(self, obj: _T) -> None:
+            self.obj = obj
+
+        def __lt__(self, other: K) -> bool:
+            return mycmp(self.obj, other.obj) < 0
+
+        def __gt__(self, other: K) -> bool:
+            return mycmp(self.obj, other.obj) > 0
+
+        def __eq__(self, other: K) -> bool:  # type: ignore[override]
+            return mycmp(self.obj, other.obj) == 0
+
+        def __le__(self, other: K) -> bool:
+            return mycmp(self.obj, other.obj) <= 0
+
+        def __ge__(self, other: K) -> bool:
+            return mycmp(self.obj, other.obj) >= 0
+
+        __hash__ = None  # type: ignore[assignment]
+
+    return K
