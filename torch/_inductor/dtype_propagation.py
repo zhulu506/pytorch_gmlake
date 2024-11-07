713255d330764e1ca138aa840def27931e0014ce
eellison <elias.ellison@gmail.com>
2024-11-06 16:37:04 -0800

    Add a few methods
    ghstack-source-id: 2c4c87ccb4073c972302e2b8a7739bf06da33624
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/139946

--- torch/_inductor/dtype_propagation.py
+++ torch/_inductor/dtype_propagation.py
@@ -3,6 +3,7 @@ import functools
 from typing import Optional, Protocol, Sequence, Tuple, TypeVar, Union
 
 import torch
+from torch._inductor.virtualized import V
 from torch._prims_common import ELEMENTWISE_TYPE_PROMOTION_KIND
 
 
@@ -185,3 +186,19 @@ class DtypePropagationOpsHandler:
     @staticmethod
     def rand(seed, offset) -> torch.dtype:
         return torch.float
+
+    @staticmethod
+    def store_reduction(name, index, value) -> torch.dtype:
+        return V.graph.get_dtype(name)
+
+    @staticmethod
+    def reduction(dtype, src_dtype, reduction_type, value) -> torch.dtype:
+        return dtype
+
+    @staticmethod
+    def store(name, index, value, mode=None) -> torch.dtype:
+        return V.graph.get_dtype(name)
+
+    @staticmethod
+    def load(name, index) -> torch.dtype:
+        return V.graph.get_dtype(name)
