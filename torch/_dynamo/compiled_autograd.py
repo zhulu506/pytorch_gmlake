1c53718ab7dac51fe265d460e4265cdbb938f7c7
Simon Fan <xmfan@meta.com>
2024-11-08 09:41:56 -0800

    [fca] support accumulate grad hooks
    ghstack-source-id: c25c6c658414200cbaf4a985a5f636a4ad12166e
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/140115

--- torch/_dynamo/compiled_autograd.py
+++ torch/_dynamo/compiled_autograd.py
@@ -419,9 +419,13 @@ class AutogradCompilerInstance:
 
         # TODO(xmfan): determine this more reliably
         if debug_name == "torch::autograd::AccumulateGrad":
-            assert len(proxy_stack) == 1
+            assert len(proxy_stack) >= 1
             assert len(proxy_inputs) == 1
+            param = stack[0]
             proxy_param = proxy_stack[0]
+            hook_num = stack[1]
+            hook_indices = stack[2:]
+            assert hook_num == len(hook_indices)
             proxy_grad = proxy_inputs[0]
             proxy_out = self.fx_tracer.create_proxy(
                 "call_function",
@@ -430,6 +434,8 @@ class AutogradCompilerInstance:
                 kwargs={},
             )
             assert num_outputs == 0
+            for idx in hook_indices:
+                self.post_acc_grad_hook(param, idx)
             return []
 
         op = ops.add(debug_name, fn)
