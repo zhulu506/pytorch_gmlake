534de8d25339065f2aa4f3796daba6fa1338647f
Ryan Guo <ryanguo99@meta.com>
2024-11-14 16:53:39 -0500

    [dynamo] Identify pre-existing captured cells by cell id rather than content id
    In `match_nested_cell`, Dynamo tried to identify pre-existing captured
    cells by `(cell_name, id(cell_contents))`. This works in most cases, but
    as the test added in this patch shows, it's not a complete solution.
    
    This patch
    1. changes `match_nested_cell` to `lookup_variable_for_captured_cell`,
       and does the lookup based on id of cell objects, not their contents.
       This requires plumbing a tuple of captured cell objects from
       different CPython versions all the way to
       `InstructionTranslator.__init__`, where we store a mapping from the
       ids of these cell objects, and use it later in
       `UserFunctionVariable.bind_args` to look for these unboxed cells.
    2. builds off (1) -- rather than using a `VariableTracker` that
       represents the content of the unboxed cells, use `ClosureVariable`,
       which enables codegen in case these cells escape as closure of a
       `NestedUserFunctionVariable`.
    
    The patch adds a regression test for each of the scenarios above:
    1. `test_write_to_cells_with_name_shadowing` where Dynamo mistakenly
       thought the program is writing to a cell captured by root frame (which
       it doesn't support atm), which resulted in
    ```
      File "/Users/ryanguo99/Documents/work/pytorch/torch/_dynamo/symbolic_convert.py", line 3340, in STORE_DEREF
        unimplemented("write to __closure__ while inlining")
      File "/Users/ryanguo99/Documents/work/pytorch/torch/_dynamo/exc.py", line 313, in unimplemented
        raise Unsupported(msg, case_name=case_name)
    torch._dynamo.exc.Unsupported: write to __closure__ while inlining
    ```
    2. `test_existing_func_that_creates_capturing_nested_func` where Dynamo
       ended up trying to codegen a `NestedUserFunctionVariable` that
       captures a cell which was also captured by the root frame, so it was
       unboxed and ends up emitting `LOAD_DEREF` rather than
       `LOAD_FAST/LOAD_CLOSURE` during codegen, resulting in
    ```
      File "/Users/ryanguo99/Documents/work/pytorch/torch/_dynamo/variables/functions.py", line 105, in _create_nested_fn
        func = FunctionType(code, f_globals, name, defaults, closure)
               ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    TypeError: arg 5 (closure) expected cell, found int
    ```
    
    ghstack-source-id: 8acbfe4c03fd88f8652e7d44957026e5dd33fb24
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/140436

--- torch/_dynamo/convert_frame.py
+++ torch/_dynamo/convert_frame.py
@@ -21,8 +21,8 @@ import typing
 import warnings
 import weakref
 from pathlib import Path
-from types import CodeType, FunctionType, ModuleType
-from typing import Any, Callable, Dict, List, Optional, Set, TypeVar, Union
+from types import CellType, CodeType, FunctionType, ModuleType
+from typing import Any, Callable, Dict, List, Optional, Set, Tuple, TypeVar, Union
 from typing_extensions import ParamSpec
 from weakref import ReferenceType
 
@@ -552,6 +552,7 @@ class ConvertFrameAssert:
                 frame.f_globals,
                 frame.f_locals,
                 frame.f_builtins,
+                frame.closure,
                 self._torchdynamo_orig_callable,
                 self._one_graph,
                 self._export,
@@ -603,6 +604,7 @@ def _compile(
     globals: Dict[str, object],
     locals: Dict[str, object],
     builtins: Dict[str, object],
+    closure: Tuple[CellType],
     compiler_fn: CompilerFn,
     one_graph: bool,
     export: bool,
@@ -646,6 +648,7 @@ def _compile(
             locals,
             globals,
             builtins,
+            closure,
             tf_mode_stack,
             code_options,
             compiler_fn,
@@ -1331,6 +1334,7 @@ def replay(filename: str) -> None:
             record.globals,
             record.locals,
             record.builtins,
+            record.closure,
             compiler_fn=eager,
             one_graph=False,
             export=False,
