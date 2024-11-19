77af31e321b995b44e3e6cedfc028b6a00dc3990
Nikhil Gupta <nikhil.gupta2@arm.com>
2024-11-19 15:11:42 +0000

    [ARM][feat]: Add KleidiAI Backend & enable 4 bit matmul operators
    Description:
    1. Integrate KleidiAI backend in aten/native
    2. Add channelwise & groupwise int4 matmul kernels
    3. Add weight & input pack kernels
    4. Add unitest for channelwise & groupwise int4 matmuls
    
    Tests:
    python test/test_linalg.py -k test__dyn_quant_pack_4bit_weight
    Ran 1 test in 0.016s
    
    OK
    
    python test/test_linalg.py -k test__dyn_quant_matmul_4bit
    Ran 8 tests in 0.077s
    
    OK
    
    python test/test_linalg.py -k test_compile_dyn_quant_matmul_4bit
    Ran 8 tests in 11.454s
    
    Signed-off-by: Nikhil Gupta <nikhil.gupta2@arm.com>
    Change-Id: I0a9c864c56a9d1b4e6179dc3059cd37b11525c8d

--- aten/src/ATen/Context.h
+++ aten/src/ATen/Context.h
@@ -117,6 +117,7 @@ class TORCH_API Context {
 
   static bool hasOpenMP();
   static bool hasMKL();
+  static bool hasKleidiAI();
   static bool hasLAPACK();
   static bool hasMKLDNN();
   static bool hasMAGMA() {
@@ -527,6 +528,10 @@ inline bool hasOpenMP() {
 
 inline bool hasMKL() {
   return globalContext().hasMKL();
+}
+
+inline bool hasKleidiAI() {
+  return globalContext().hasKleidiAI();
 }
 
 inline bool hasLAPACK() {
