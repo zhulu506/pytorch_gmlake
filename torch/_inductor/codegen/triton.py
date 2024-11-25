78a674ec2416bbdfccfb719f0056c67fc470d5d2
Xinran / Allan Rui <arui@meta.com>
2024-11-25 08:54:41 -0800

    [Inductor/Triton] Upcast FP16/BF16 math reductions to FP32 (#141052)
    Summary:
    
    Triton compiler does not automatically promote fp16/bf16 reductions to fp32  accumulation. This will result in significant accuracy issue.
    
    This diff will upcast the input to FP32 for all math reductions `["welford_reduce", "welford_combine", "prod", "sum"]`
    
    Test Plan:
    CI
    ```
    python test/inductor/test_torchinductor.py TritonCodeGenTests.test_low_precision_reduction
    ```
    
    Reviewed By: blaine-rister
    
    Differential Revision: D65965032

--- torch/_inductor/codegen/triton.py
+++ torch/_inductor/codegen/triton.py
@@ -2146,6 +2146,14 @@ class TritonKernel(SIMDKernel):
         reduction_type: ReductionType,
         value: Union[CSEVariable, Tuple[CSEVariable, ...]],
     ) -> Union[CSEVariable, Tuple[CSEVariable, ...]]:
+        if dtype in [torch.float16, torch.bfloat16,]:
+            # Math reductions in FP16/BF16 is less accurate because Triton compiler does not
+            # automatically promote to FP32 for accumulation. Additionally, max/min reudctions
+            # do not support FP16/BF16. We manually promote to FP32 here.
+            value = ops.to_dtype(value, torch.float32)
+            src_dtype = torch.float32
+            dtype = torch.float32
+
         assert self.inside_reduction
         masks = OrderedSet(f"{tree.prefix}mask" for tree in self.range_trees)
         self.filter_masks(masks)
