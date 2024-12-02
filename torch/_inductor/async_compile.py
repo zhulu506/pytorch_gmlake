36c9f9fd88d3d559df73b57d1a7346dbc1dc2e94
Colin L. Rice <clr@fb.com>
2024-12-02 16:28:55 -0700

    _inductor: Add WaitCounter for async_compile.wait calls.
    This will start recording how long these async_compile.wait calls take.
    
    Note that we want to just unify dynamo_timed in the long term.
    
    ghstack-source-id: 9acf032b5d7051a854f6a6fe468ccc8640e2153a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/141379

--- torch/_inductor/async_compile.py
+++ torch/_inductor/async_compile.py
@@ -38,6 +38,7 @@ from torch._inductor.runtime.compile_tasks import (
     _worker_compile_triton,
 )
 from torch.hub import _Faketqdm, tqdm
+from torch.monitor import _WaitCounter
 from torch.utils._triton import has_triton_package
 
 
@@ -232,7 +233,8 @@ class AsyncCompile:
                 ),
             )
         else:
-            kernel.precompile()
+            with _WaitCounter("pytorch.async_compile.precompile").guard():
+                kernel.precompile()
             return kernel
 
     def multi_kernel(self, *args, **kwargs) -> Any:
@@ -299,7 +301,9 @@ class AsyncCompile:
             return LambdaFuture(get_result)
 
     def wait(self, scope: Dict[str, Any]) -> None:
-        with dynamo_timed("async_compile.wait", log_pt2_compile_event=True):
+        with dynamo_timed(
+            "async_compile.wait", log_pt2_compile_event=True
+        ), _WaitCounter("pytorch.async_compile.wait").guard():
             num_kernels = len(
                 [
                     value
