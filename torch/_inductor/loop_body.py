5e26ed4bf07c4893ebe43c76515548290322b52a
Tom Ritchford <tom@swirly.com>
2024-12-02 14:54:08 +0000

    [inductor] Update type annotations to Python3.9 for _inductor/*.py
    ghstack-source-id: 95c990c55a549bd833a6ef5f9325b4cae660fb9a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/141871

--- torch/_inductor/loop_body.py
+++ torch/_inductor/loop_body.py
@@ -5,18 +5,9 @@ import collections
 import functools
 import itertools
 import re
+from collections.abc import Sequence
 from enum import auto, Enum
-from typing import (
-    Any,
-    Callable,
-    Dict,
-    List,
-    NamedTuple,
-    Optional,
-    Sequence,
-    Tuple,
-    TypeVar,
-)
+from typing import Any, Callable, Dict, List, NamedTuple, Optional, Tuple, TypeVar
 
 import sympy
 
@@ -72,8 +63,8 @@ class LightTracer(TracerBase):
 
 class MemoryEntry(NamedTuple):
     index_name: str  # LoopBody.indexing_exprs[index_name]
-    buffer_name: Optional[str]
-    mode: Optional[str]  # V.ops.store(..., mode=mode)
+    buffer_name: str | None
+    mode: str | None  # V.ops.store(..., mode=mode)
 
 
 class MemoryUsageType(Enum):
@@ -93,14 +84,14 @@ class LoopBody:
     indexing simplifications and makes it easier to analyze loop bodies.
     """
 
-    indexing_exprs: Dict[str, sympy.Expr]
-    indexing_exprs_name: Dict[sympy.Expr, str]
-    submodules: Dict[str, Any]
-    subblocks: Dict[str, LoopBodyBlock]
-    indirect_vars: List[sympy.Symbol]
-    indirect_var_ranges: Dict[sympy.Symbol, sympy.Expr]
+    indexing_exprs: dict[str, sympy.Expr]
+    indexing_exprs_name: dict[sympy.Expr, str]
+    submodules: dict[str, Any]
+    subblocks: dict[str, LoopBodyBlock]
+    indirect_vars: list[sympy.Symbol]
+    indirect_var_ranges: dict[sympy.Symbol, sympy.Expr]
     root_block: LoopBodyBlock
-    memory_usage: Dict[MemoryUsageType, List[MemoryEntry]]
+    memory_usage: dict[MemoryUsageType, list[MemoryEntry]]
     op_counts: collections.Counter[str]
 
     def __init__(self, fn, args, var_ranges, iter_vars, reduce_vars):
@@ -130,7 +121,7 @@ class LoopBody:
         self.submodules = {"get_index": self.get_index}
         self.subblocks = {}
         self.indirect_vars = []
-        self.indirect_var_ranges: Dict[sympy.Symbol, sympy.Expr] = {}
+        self.indirect_var_ranges: dict[sympy.Symbol, sympy.Expr] = {}
         self.memory_usage = {t: [] for t in MemoryUsageType}
         self.op_counts = collections.Counter()
         self.root_block = LoopBodyBlock(self, fn, args)  # traces
@@ -349,8 +340,8 @@ class LoopBody:
         self,
         expr: sympy.Expr,
         mtype: MemoryUsageType,
-        buffer_name: Optional[str] = None,
-        mode: Optional[str] = None,
+        buffer_name: str | None = None,
+        mode: str | None = None,
     ):
         name = self.indexing_exprs_name.get(expr)
         if not name:
@@ -443,7 +434,7 @@ class LoopBodyBlock:
     operations will manifest as an extra LoopBodyBlock.
     """
 
-    def __init__(self, body: LoopBody, fn: Callable[..., Any], args: List[Any]):
+    def __init__(self, body: LoopBody, fn: Callable[..., Any], args: list[Any]):
         self.body = body
 
         def add_index(expr: sympy.Expr, mtype: MemoryUsageType, **kwargs):
@@ -500,12 +491,12 @@ class LoopBodyBlock:
             def bucketize(
                 self,
                 values: T,
-                boundaries: Tuple[str, sympy.Expr, sympy.Expr, sympy.Expr],
+                boundaries: tuple[str, sympy.Expr, sympy.Expr, sympy.Expr],
                 boundary_indices: T,
                 indexing_dtype: torch.dtype,
                 right: bool,
-                sorter: Optional[Tuple[str, sympy.Expr]] = None,
-                sorter_indices: Optional[T] = None,
+                sorter: tuple[str, sympy.Expr] | None = None,
+                sorter_indices: T | None = None,
             ) -> T:
                 """
                 See [Note: Inductor bucketize op]
@@ -562,7 +553,7 @@ class LoopBodyBlock:
             def scan(
                 dtype_proxy,
                 combine_fn: Callable[
-                    [Tuple[Any, ...], Tuple[Any, ...]], Tuple[Any, ...]
+                    [tuple[Any, ...], tuple[Any, ...]], tuple[Any, ...]
                 ],
                 value_proxy,
             ):
